59ffbecbf391bd15b90c617374606827a338211a
dobinrutis <dobin@broken.ch>
2021-05-08 19:48:48 +0200

    refactoring: split main loop, to lazer

---
+++ lazer.py
@@ -0,0 +1,179 @@
+import cv2 as cv
+import imutils
+from collections import deque
+import numpy as np
+from skimage.metrics import structural_similarity as ssim
+import os.path
+from utils import *
+from pathlib import Path
+import yaml
+import argparse
+
+class RecordedHit(object):
+    def __init__(self):
+        self.center = None
+        self.x = 0
+        self.y = 0
+        self.radius = 0
+        self.mask = None
+
+    def toDict(self):
+        me = {
+            'center': self.center,
+            'x': self.x,
+            'y': self.y,
+            'radius': self.radius
+        }
+        return me
+
+# from ball_tracking.py
+def findContours(mask):
+    res = []
+
+    # find contours in the mask and initialize the current
+    # (x, y) center of the ball
+    cnts = cv.findContours(mask.copy(), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    center = None
+
+    # only proceed if at least one contour was found
+    if len(cnts) > 0:
+        # find the largest contour in the mask, then use
+        # it to compute the minimum enclosing circle and
+        # centroid
+        c = max(cnts, key=cv.contourArea)
+        ((x, y), radius) = cv.minEnclosingCircle(c)
+        M = cv.moments(c)
+        center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+
+        # only proceed if the radius meets a minimum size
+        if radius > 10:
+            print("Found dot with radius " + str(radius) + "at  X:" + str(x) + "  Y:" + str(y))
+
+            recordedHit = RecordedHit()
+            recordedHit.x = x
+            recordedHit.y = y
+            recordedHit.center = center
+            recordedHit.radius = radius
+            recordedHit.mask = mask.copy()
+            res.append(recordedHit)
+
+    return res
+
+
+def diff(mask, previousMask, frame):
+    # compute the Structural Similarity Index (SSIM) between the two
+    # images, ensuring that the difference image is returned
+    (score, diff) = ssim(mask, previousMask, full=True)
+    diff = (diff * 255).astype("uint8")
+    #print("SSIM: {}".format(score))
+
+    # threshold the difference image, followed by finding contours to
+    # obtain the regions of the two input images that differ
+    thresh = cv.threshold(diff, 0, 255, cv.THRESH_BINARY_INV | cv.THRESH_OTSU)[1]
+    cnts = cv.findContours(thresh.copy(), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+
+    # loop over the contours
+    for c in cnts:
+        # compute the bounding box of the contour and then draw the
+        # bounding box on both input images to represent where the two
+        # images differ
+        (x, y, w, h) = cv.boundingRect(c)
+        cv.rectangle(diff, (x, y), (x + w, y + h), (0, 0, 255), 2)
+        #cv.rectangle(imageB, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+    #cv.imshow('diff', diff)
+    return diff
+
+#
+# Order:
+# - init*
+# - nextFrame()
+# - getContours()
+# - displayFrame()
+# - release()
+class Lazer(object):
+    def __init__(self, showVid):
+        self.capture = None
+        self.frame = None
+        self.mask = None
+        self.previousMask = None
+        self.frameNr = -1  # so it is 0 the first iteration
+        self.lastFoundFrameNr = 0
+        self.showVid = showVid
+        
+        self.graceTime = 10  # How many frames between detections
+
+
+    def initFile(self, filename):
+        if not os.path.isfile(filename):
+            print("File not found")
+            return
+
+        self.capture = cv.VideoCapture(filename)
+
+
+    def nextFrame(self):
+        self.frameNr += 1
+        self.previousMask = self.mask
+
+        isTrue, self.frame = self.capture.read()
+        if not isTrue:
+            return False
+
+        # Frame: rescale it
+        self.frame = rescaleFrame(self.frame)
+        # Mask: grey
+        self.mask = toGrey(self.frame)
+        # Mask: make it a bit sharper
+        self.mask = sharpoon(self.mask)
+        # Mask: force super low brightness high contrast
+        self.mask = apply_brightness_contrast(self.mask, -126, 115)
+        # mask = apply_brightness_contrast(mask, -127, 116)
+
+        return True
+
+
+    def getContours(self, addIndicator=True):
+        # wait a bit between detections
+        if (self.frameNr - self.lastFoundFrameNr) < self.graceTime:
+            return []
+
+        recordedHits = findContours(self.mask)
+        if len(recordedHits) > 0:
+            self.lastFoundFrameNr = self.frameNr
+
+        for recordedHit in recordedHits:
+            # also check the diff
+            d = diff(self.mask, self.previousMask, self.frame)
+            if self.showVid:
+                cv.imshow('Diff', d)
+            
+            if addIndicator:
+                # add visual indicators to both frame and mask
+                cv.circle(self.frame, (int(recordedHit.x), int(recordedHit.y)), int(recordedHit.radius), (255, 0, 0), 2)
+                cv.circle(self.frame, recordedHit.center, 5, (0, 0, 0), -1)
+
+                cv.circle(self.mask, (int(recordedHit.x), int(recordedHit.y)), int(recordedHit.radius), (255, 0, 0), 2)
+                cv.circle(self.mask, recordedHit.center, 5, (0, 0, 0), -1)
+
+            # write all the pics
+            #cv.imwrite(filenameBase + "_" + str(frameNr) + "_cont.jpg", recordedHit.mask)
+            #cv.imwrite(filenameBase + "_" + str(frameNr) + "_frame.jpg", frame)
+            #cv.imwrite(filenameBase + "_" + str(frameNr) + "_diff.jpg", d)
+            #with open(filenameBase + "_" + str(frameNr) + "_info.yaml", 'w') as outfile:
+            #    yaml.dump(recordedHit.toDict(), outfile)
+
+        return recordedHits
+
+
+    def displayFrame(self):
+        cv.imshow('Video', self.frame)
+        cv.imshow('Mask', self.mask)
+
+
+    def release(self):
+        self.capture.release()
+        if self.showVid:
+            cv.destroyAllWindows()
--- xtarget.py
+++ xtarget.py
@@ -7,89 +7,8 @@ import os.path
 from utils import *
 from pathlib import Path
 import yaml
-
-debug = True
-
-
-class RecordedHit(object):
-    def __init__(self):
-        self.center = None
-        self.x = 0
-        self.y = 0
-        self.radius = 0
-        self.mask = None
-
-    def toDict(self):
-        me = {
-            'center': self.center,
-            'x': self.x,
-            'y': self.y,
-            'radius': self.radius
-        }
-        return me
-
-
-# from ball_tracking.py
-def findContours(mask):
-    res = []
-
-    # find contours in the mask and initialize the current
-    # (x, y) center of the ball
-    cnts = cv.findContours(mask.copy(), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-    cnts = imutils.grab_contours(cnts)
-    center = None
-
-    # only proceed if at least one contour was found
-    if len(cnts) > 0:
-        # find the largest contour in the mask, then use
-        # it to compute the minimum enclosing circle and
-        # centroid
-        c = max(cnts, key=cv.contourArea)
-        ((x, y), radius) = cv.minEnclosingCircle(c)
-        M = cv.moments(c)
-        center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-
-        # only proceed if the radius meets a minimum size
-        if radius > 10:
-            print("Found dot with radius " + str(radius) + "at  X:" + str(x) + "  Y:" + str(y))
-
-            recordedHit = RecordedHit()
-            recordedHit.x = x
-            recordedHit.y = y
-            recordedHit.center = center
-            recordedHit.radius = radius
-            recordedHit.mask = mask.copy()
-            res.append(recordedHit)
-
-    return res
-
-
-def diff(mask, previousMask, frame):
-    # compute the Structural Similarity Index (SSIM) between the two
-    # images, ensuring that the difference image is returned
-    (score, diff) = ssim(mask, previousMask, full=True)
-    diff = (diff * 255).astype("uint8")
-    #print("SSIM: {}".format(score))
-
-    # threshold the difference image, followed by finding contours to
-    # obtain the regions of the two input images that differ
-    thresh = cv.threshold(diff, 0, 255, cv.THRESH_BINARY_INV | cv.THRESH_OTSU)[1]
-    cnts = cv.findContours(thresh.copy(), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-    cnts = imutils.grab_contours(cnts)
-
-    # loop over the contours
-    for c in cnts:
-        # compute the bounding box of the contour and then draw the
-        # bounding box on both input images to represent where the two
-        # images differ
-        (x, y, w, h) = cv.boundingRect(c)
-        cv.rectangle(diff, (x, y), (x + w, y + h), (0, 0, 255), 2)
-        #cv.rectangle(imageB, (x, y), (x + w, y + h), (0, 0, 255), 2)
-
-    #cv.imshow('diff', diff)
-    return diff
-
-
+import argparse
+from lazer import RecordedHit, Lazer
 
 
 def doTests():
@@ -98,90 +17,55 @@ def doTests():
 
 
 def testcase(filename):
-    pass
-
-
-def getFrame(capture, showVid):
-    isTrue, frame = capture.read()
-    if not isTrue:
-        return None, None
-
-    # rescale it        
-    frame = rescaleFrame(frame)
-    # grey
-    mask = toGrey(frame)
-    # make it a bit sharper
-    mask = sharpoon(mask)
-    # force super low brightness high contrast
-    mask = apply_brightness_contrast(mask, -126, 115)
-    # mask = apply_brightness_contrast(mask, -127, 116)
-    if debug and showVid:
-        cv.imshow('Mask', mask)
-
-    return frame, mask
-
-
-def analyzeVideo(filename, showVid=True):
     print("Analyzing file: " + filename)
-    if not os.path.isfile(filename):
-        print("File not found")
-        return
+    lazer = Lazer(showVid=False)
+    lazer.initFile("tests/" + filename + ".mp4")
 
-    filenameBase = os.path.splitext(filename)[0]
-
-    # Reading Videos
-    capture = cv.VideoCapture(filename)
-
-    previousMask = None
-    frameNr = 0
-    lastFoundFrameNr = 0
     while True:
-        frame, mask = getFrame(capture, showVid)
-        if frame is None:
+        hasFrame = lazer.nextFrame()
+        if not hasFrame:
             break
 
         # find contours and visualize it in the main frame
-        if (frameNr - lastFoundFrameNr) > 10:
-            recordedHits = findContours(mask)
+        contours = lazer.getContours()
+        if len(contours) > 0:
+            print(lazer.frameNr)
 
-            for recordedHit in recordedHits:
-                lastFoundFrameNr = frameNr
+    lazer.release()
 
-                # also check the diff
-                d = diff(mask, previousMask, frame)
-                if showVid:
-                    cv.imshow('Diff', d)
-                
-                # add visual indicators
-                cv.circle(frame, (int(recordedHit.x), int(recordedHit.y)), int(recordedHit.radius), (255, 0, 0), 2)
-                cv.circle(frame, recordedHit.center, 5, (0, 0, 0), -1)
-                cv.circle(mask, (int(recordedHit.x), int(recordedHit.y)), int(recordedHit.radius), (255, 0, 0), 2)
-                cv.circle(mask, recordedHit.center, 5, (0, 0, 0), -1)
 
-                # write all the pics
-                #cv.imwrite(filenameBase + "_" + str(frameNr) + "_cont.jpg", recordedHit.mask)
-                #cv.imwrite(filenameBase + "_" + str(frameNr) + "_frame.jpg", frame)
-                #cv.imwrite(filenameBase + "_" + str(frameNr) + "_diff.jpg", d)
-                #with open(filenameBase + "_" + str(frameNr) + "_info.yaml", 'w') as outfile:
-                #    yaml.dump(recordedHit.toDict(), outfile)
+def analyzeVideo(filename):
+    print("Analyzing file: " + filename)
+    lazer = Lazer(showVid=True)
+    lazer.initFile(filename)
 
-        # show it
-        if showVid:
-            cv.imshow('Video', frame)
+    while True:
+        hasFrame = lazer.nextFrame()
+        if not hasFrame:
+            break
 
-            # check for end
-            if cv.waitKey(20) & 0xFF==ord('d'):
-                break
+        # find contours and visualize it in the main frame
+        contours = lazer.getContours()
+        lazer.displayFrame()
 
-        frameNr+=1
-        previousMask = mask
+        # check for end
+        if cv.waitKey(20) & 0xFF==ord('d'):
+            break
 
-    capture.release()
-    cv.destroyAllWindows()
+    lazer.release()
 
 
 def main():
-    analyzeVideo('tests/test2.mp4', showVid=True)
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-v", "--video", help="video", action='store_true')
+    ap.add_argument("-t", "--test", help="test", action='store_true')
+    args = ap.parse_args()
+
+    if args.video:
+        analyzeVideo('tests/test2.mp4')
+    elif args.test:
+        doTests()
+
 
 if __name__ == "__main__":
     main()
